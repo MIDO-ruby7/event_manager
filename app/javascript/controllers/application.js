8735704c381201d1ef7482d5fdaaf5b3c8fed6ed
Midori Takahashi <mintgreen7programming@gmail.com>
2023-05-11 11:48:31 +0900

    rails new event-manager -j esbuild, rails g controller site index, ./bin/rails g model Event

---
+++ app/javascript/controllers/application.js
@@ -0,0 +1,12 @@
+import { Application } from "@hotwired/stimulus"
+
+import React from "react";
+import { createRoot } from "react-dom/client";
+import HelloMessage from "../components/App";
+
+const container = document.getElementById("root");
+const root = createRoot(container);
+
+document.addEventListener("DOMContentLoaded", () => {
+  root.render(<HelloMessage name="World" />);
+});
