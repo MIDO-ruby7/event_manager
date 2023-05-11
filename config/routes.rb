b56a89c0020e86c9abf25e8d2faa2b42ae9ffdef
Midori Takahashi <mintgreen7programming@gmail.com>
2023-05-11 15:03:43 +0900

    ADD:events api

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
   root to: 'site#index'
+
+  namespace :api, format: 'json' do
+    namespace :v1 do
+      resources :events, only: %i[index show create destroy update]
+    end
+  end
 end
